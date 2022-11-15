10a45c3740cc456a52e3e5d413e44a946553efca
ashfaqueahmeddl <ashfaq.ahamad@gmail.com>
2022-11-15 11:48:32 +0530

    git command commit

--- java/abc.java
+++ java/abc.java
@@ -2,3 +2,8 @@ hello world
 
 hello again
 test git user
+
+        // switing the git user from terminak
+git config --global user.name "ashfaqueahmeddl"
+// check git user name from terminal
+git config user.name
